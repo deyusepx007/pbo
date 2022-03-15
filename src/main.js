e12cfa0675c893f412c5edaf82e7e014fade8810
deyusepx007 <93562957+deyusepx007@users.noreply.github.com>
2022-03-15 13:20:04 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import VueSupabase from "vue-supabase";
+
+Vue.use(VueSupabase, {
+  supabaseUrl: "https://jzfnncrdkbyepmunprzj.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyOTk4NTMwLCJleHAiOjE5NTg1NzQ1MzB9.hDUVMw_WkDki2c7LcrUwsYux2HXQAMEW3VXYLSZAZAU",
+  supabaseOptions: {},
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
